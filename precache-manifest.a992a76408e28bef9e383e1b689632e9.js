fc583bfa48fdd8d09cef174375a5e72d2b6781b0
Alexis Lucas <alexis.lucas1@viacesi.fr>
2018-11-10 01:38:28 +0000

    Updates

---
+++ precache-manifest.a992a76408e28bef9e383e1b689632e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c7288e893ac69d3a9f1",
+    "url": "/lithoprojets/static/js/runtime~main.4c7288e8.js"
+  },
+  {
+    "revision": "94b157c05990881bfa20",
+    "url": "/lithoprojets/static/js/main.94b157c0.chunk.js"
+  },
+  {
+    "revision": "067d7e8e937d8b00a3cd",
+    "url": "/lithoprojets/static/js/1.067d7e8e.chunk.js"
+  },
+  {
+    "revision": "0fa0d44fc1358cb887a62c3d2b5d70f7",
+    "url": "/lithoprojets/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/lithoprojets/precache-manifest.2d42f1903862dfaf905255d10fd8a0cd.js"
+  "/lithoprojets/precache-manifest.a992a76408e28bef9e383e1b689632e9.js"
 );
 
 workbox.clientsClaim();
